ae2249e1a205705eeb6d050fea599756515f5a2a
Grigory56 <95852762+Grigory56@users.noreply.github.com>
2024-09-23 06:58:09 +0700

    Add files via upload

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,5 +1,12 @@
 from random import random
 
-x=int(random()*20)
-
-print(x)
+for x in range(3,21):
+    rezult = ''
+    for  i in range(1,x):
+        for j in range(i+1,20):
+            if x % (i+j) == 0:
+                rezult += str(i)
+                rezult += str(j)
+            else:
+                    continue
+    print (x ,'-',rezult)
